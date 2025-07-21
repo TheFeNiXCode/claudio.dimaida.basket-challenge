2144abb3499d37dc64a094104c8f8754708439c5
Claudio DI Maida <claudio.dimaida@gmail.com>
2025-07-21 14:01:52 +0200

    Ball shooting with inputs
    Combination of mouse and touch input with ball physics to score a basket

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] PlayerSettings settings;
+    [SerializeField] Cloth Net;
 
     protected Transform myTransform { get; private set; }
 
@@ -21,7 +22,10 @@ public class PlayerManager : MonoBehaviour
     private Vector2 swipeStart;
     private Vector2 swipeEnd;
     private float swipeStartTime;
+    private Vector2 swipeDirection;
 
+    private float forceMultiplier = 1.0f;
+    private float precision = 1.0f;
 
     protected virtual void Awake()
     {
@@ -35,9 +39,62 @@ public class PlayerManager : MonoBehaviour
     private void Update()
     {
         ableToShoot();
+
     }
 
-    
+    void HandleSwipe(Vector2 start, Vector2 end, float duration, GameObject ballInstance)
+    {
+        if (duration > maxSwipeTime) return;
+
+        Vector2 swipe = end - start;
+
+        if (swipe.magnitude < 50f)
+        {
+            // Swipe troppo corto, consideralo come click e applica una forza minima
+            swipeDirection = Vector2.up;
+            forceMultiplier = minSwipeForce;
+            precision = minPrecision; // opzionale: tiro impreciso nei semplici click
+            gameObject.GetComponent<BallShooter>().Shoot(ballInstance, forceMultiplier, precision, swipeStart, swipeEnd);
+            return;
+        }
+
+        swipeDirection = swipe.normalized;
+
+        float swipeStrength = Mathf.Clamp(swipe.magnitude / Screen.height, 0f, 1f);
+
+        forceMultiplier = Mathf.Lerp(minSwipeForce, maxSwipeForce, swipeStrength);
+        precision = Mathf.Lerp(minPrecision, maxPrecision, swipeStrength);
+
+        // Curvatura = quanto l’utente si è mosso lateralmente rispetto allo swipe verticale ideale
+        float lateralDeviation = Mathf.Abs(swipe.x);
+        float verticalMovement = Mathf.Abs(swipe.y);
+        float curvatureFactor = Mathf.Clamp01(lateralDeviation / (verticalMovement + 0.01f));
+
+        curvatureFactor *= maxSpinCurveFactor;
+
+        gameObject.GetComponent<BallShooter>().Shoot(ballInstance, forceMultiplier, precision, swipeStart, swipeEnd, curvatureFactor);
+    }
+
+
+    private GameObject instanceBall()
+    {
+        GameObject ball = settings.ballSelected();
+        if (ball)
+        {
+            GameObject ballInstance = Instantiate(ball, myTransform);
+            ballInstance.transform.localPosition = Vector3.zero;
+
+            SphereCollider ballCollider = ballInstance.GetComponent<SphereCollider>();
+            Net.sphereColliders = new ClothSphereColliderPair[]
+            {
+                new ClothSphereColliderPair(ballCollider)
+            };
+
+            return ballInstance;
+        }
+        return null;
+    }
+
     private void ableToShoot()
     {
 #if UNITY_ANDROID || UNITY_IOS
@@ -62,6 +119,7 @@ public class PlayerManager : MonoBehaviour
                     Debug.Log("Swipe ignorato: non è verso l'alto");
                     return;
                 }
+                HandleSwipe(swipeStart, swipeEnd, swipeDuration, instanceBall());
             }
         }
         
@@ -83,6 +141,9 @@ public class PlayerManager : MonoBehaviour
                 Debug.Log("Swipe ignorato: non è verso l'alto");
                 return;
             }
+
+            HandleSwipe(swipeStart, swipeEnd, swipeDuration, instanceBall());
+
         }
 #endif
     }
