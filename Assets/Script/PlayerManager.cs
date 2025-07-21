d434d96835a27901061eb2d178709d1e3fb2863d
Claudio DI Maida <claudio.dimaida@gmail.com>
2025-07-21 12:50:36 +0200

    Swipe detection
    Gesture input detection for swiping via mouse or touch (in the mobile version)

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,22 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] PlayerSettings settings;
+
     protected Transform myTransform { get; private set; }
 
-    // Start is called before the first frame update
+    [Header("Input Swipe")]
+    public float minSwipeForce = 5f;
+    public float maxSwipeForce = 20f;
+    public float minPrecision = 0.6f;
+    public float maxPrecision = 1.0f;
+    public float maxSwipeTime = 1f;
+    [Range(0f, 1f)]
+    public float maxSpinCurveFactor = 1.0f;
+
+    private Vector2 swipeStart;
+    private Vector2 swipeEnd;
+    private float swipeStartTime;
+
 
     protected virtual void Awake()
     {
@@ -15,16 +29,62 @@ public class PlayerManager : MonoBehaviour
     }
     void Start()
     {
-        GameObject ball = settings.ballSelected();
-        if (ball)
-        {
-            Instantiate(ball, myTransform).transform.localPosition = Vector3.zero;
-        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ableToShoot();
     }
+
+    
+    private void ableToShoot()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                swipeStart = touch.position;
+                swipeStartTime = Time.time;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                swipeEnd = touch.position;
+                float swipeDuration = Time.time - swipeStartTime;
+                
+                Vector2 swipe = swipeEnd - swipeStart;
+                if (swipe.y < 0 || swipe.y < Mathf.Abs(swipe.x))
+                {
+                    Debug.Log("Swipe ignorato: non è verso l'alto");
+                    return;
+                }
+            }
+        }
+        
+#else
+        if (Input.GetMouseButtonDown(0))
+        {
+            swipeStart = Input.mousePosition;
+            swipeStartTime = Time.time;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            swipeEnd = Input.mousePosition;
+            float swipeDuration = Time.time - swipeStartTime;
+
+            Vector2 swipe = swipeEnd - swipeStart;
+
+            if (swipe.y < 0 || swipe.y < Mathf.Abs(swipe.x))
+            {
+                Debug.Log("Swipe ignorato: non è verso l'alto");
+                return;
+            }
+        }
+#endif
+    }
+
 }
